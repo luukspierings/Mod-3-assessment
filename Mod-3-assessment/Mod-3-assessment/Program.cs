8ecf2668758f16107c4418eb937b64cc80d7941c
luukspierings <lcpspier@avans.nl>
2015-10-19 11:16:30 +0200

    classes

--- Mod-3-assessment/Mod-3-assessment/Program.cs
+++ Mod-3-assessment/Mod-3-assessment/Program.cs
@@ -14,7 +14,7 @@ namespace Mod_3_assessment
 
 
             
-            // While no one is pressing any keys just keep on beeping
+            
             while (!Console.KeyAvailable)
             {
                 Console.Beep(3200,100);
