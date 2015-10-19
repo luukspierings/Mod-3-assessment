59ae60ffeffbba8753e9295cf59385e83b020329
luukspierings <lcpspier@avans.nl>
2015-10-19 11:47:53 +0200

    ok alles

---
+++ Mod-3-assessment/Mod-3-assessment/Domain/Ship.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod_3_assessment.Domain
+{
+    class Ship
+    {
+    }
+}
