05be5b2c1561d4675a3ed2bc9d6b526761dbf818
luukspierings <lcpspier@avans.nl>
2015-10-19 10:09:53 +0200

    aanmaken project

---
+++ Mod-3-assessment/Mod-3-assessment/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod_3_assessment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
